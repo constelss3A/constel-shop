318f419ccbbdc42eb3f7a14746c7146f68e50859
Yerich Sales <yerichsalles@gmail.com>
2026-04-01 07:23:09 -0300

    Empresa: inclusão das propriedades de configuração

--- src/app/modulos/administrativo/empresa/empresa.ts
+++ src/app/modulos/administrativo/empresa/empresa.ts
@@ -2,4 +2,22 @@ export class Empresa {
   id: string;
   codigo: string;
   nome: string;
+  imagem: string;
+  empresaConfiguracao: {
+    parametros: {
+      visual: {
+        cor: string;
+        cardapio: {
+          ativo: boolean;
+          faixa: string;
+        },
+      },
+      internet: {
+        site: string;
+        facebook: string;
+        youtube: string;
+        x: string;
+      },
+    },
+  }
 }
